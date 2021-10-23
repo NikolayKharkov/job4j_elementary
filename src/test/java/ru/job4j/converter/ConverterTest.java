f0892206373af8421571eaaca6cea20739f3f221
NikolayKharkov <vtiko564@gmail.com>
2021-10-23 15:24:56 +0300

    1. Модульные тесты. [#159435]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -17,7 +17,7 @@ public class ConverterTest {
     @Test
     public void whenConvert180RblThen3Dlr() {
         float in = 180;
-        float expected = 4;
+        float expected = 3;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
