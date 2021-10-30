b9ac87c4af219f05f443d0b5b9fbfb5f6ad2c4be
NikolayKharkov <vtiko564@gmail.com>
2021-10-30 14:11:01 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/condition/Games.java
+++ src/main/java/ru/job4j/condition/Games.java
@@ -1,8 +1,8 @@
 package ru.job4j.condition;
 
 public class Games {
-    public static void permission(boolean allowByParent, boolean hasMoney) {
-        if (allowByParent && hasMoney) {
+    public static void permission(boolean allow, boolean hasMoney) {
+        if (allow && hasMoney) {
             System.out.println("I can go to the game club.");
         } else {
             System.out.println("I can't.");
